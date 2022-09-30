a122361ae02c29eabaf0ae276b9706451ea701e1
Doğanur Turan <71984827+DoganurTuran@users.noreply.github.com>
2022-10-01 00:14:40 +0300

    Add files via upload

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Bol(8, 4);
+		System.out.println(sonuc);
+
+	}
+
+}
